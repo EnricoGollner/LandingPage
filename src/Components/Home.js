46ae5298dd4ba410315a0862c468634e0a8a5216
EnricoGollner <enricogollner.tech@gmail.com>
2024-11-06 16:30:20 -0300

    Project started

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import Reacts from "react";
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <Navbar />
+        </div>
+    );
+}
+
+export default Home;
